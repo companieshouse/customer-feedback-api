e5f91715fcbb27f6303564a4fd591a766e45c2db
NeilShadrach <neil.shadrach@corryn.com>
2023-11-21 11:48:33 +0000

    CC-869 Make return OK explicit

--- src/main/java/uk/gov/companieshouse/customerfeedbackapi/controller/HealthCheckController.java
+++ src/main/java/uk/gov/companieshouse/customerfeedbackapi/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package uk.gov.companieshouse.customerfeedbackapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
   @GetMapping("/customer-feedback/healthcheck")
-  public String healthcheck() {
-    return "Customer Feedback API Service is healthy";
+  public ResponseEntity<String> healthcheck() {
+    return new ResponseEntity("Customer Feedback API Service is healthy", HttpStatus.OK);
   }
 }
