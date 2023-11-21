e7dd011ed4b885c21aa7e702a7f2ca145f91b8d8
NeilShadrach <neil.shadrach@corryn.com>
2023-11-21 11:42:22 +0000

    CC-869 Remove commented-out code

--- src/main/java/uk/gov/companieshouse/customerfeedbackapi/repository/CustomerFeedbackRepository.java
+++ src/main/java/uk/gov/companieshouse/customerfeedbackapi/repository/CustomerFeedbackRepository.java
@@ -1,13 +1,10 @@
 package uk.gov.companieshouse.customerfeedbackapi.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-// import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.gov.companieshouse.customerfeedbackapi.model.dao.CustomerFeedbackDAO;
 
 @Repository
 public interface CustomerFeedbackRepository extends MongoRepository<CustomerFeedbackDAO, String> {
 
-  // @Query("{transaction_id:'?0'}")
-  // CustomerFeedbackDAO findByTransactionId(String transactionId);
 }
