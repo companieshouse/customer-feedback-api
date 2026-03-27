e8af6e16b7f9070dd140fce5436747e03dadace7
AdeHarrison-ch <aharrison2@companieshouse.gov.uk>
2026-03-27 13:18:31 +0000

    ASM-1899 Use Mongo 7.0.5

--- src/test/java/uk/gov/companieshouse/customerfeedbackapi/integration/CustomerFeedbackControllerIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/customerfeedbackapi/integration/CustomerFeedbackControllerIntegrationTest.java
@@ -27,7 +27,7 @@ class CustomerFeedbackControllerIntegrationTest {
 
     @Container
     @ServiceConnection
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.31");
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
 
     @Autowired
     private MockMvc mvc;
