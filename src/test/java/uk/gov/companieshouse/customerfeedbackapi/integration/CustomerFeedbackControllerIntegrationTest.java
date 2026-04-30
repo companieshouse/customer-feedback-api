8d2b2cb7d062c8d9be7a3f9558e3c5d93dbd67fd
pparlett <pparlett@companieshouse.gov.uk>
2026-04-30 10:36:35 +0100

    updated springboot to 4.0.4

--- src/test/java/uk/gov/companieshouse/customerfeedbackapi/integration/CustomerFeedbackControllerIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/customerfeedbackapi/integration/CustomerFeedbackControllerIntegrationTest.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
